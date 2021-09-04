59d47b587309c5a57e2f04040c6bad4d7a5a4c2e
Pawan Kumar <losermyluckwithyou007@gmail.com>
2021-09-04 15:24:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/multipleactivitydemo/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.multipleactivitydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    public void goBack(View view) {
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+    }
+}
